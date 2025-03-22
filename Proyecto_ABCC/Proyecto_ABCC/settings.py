163194f7b4559250223002e348b6d7c888e3d2a9
anhiGF <153327217+anhiGF@users.noreply.github.com>
2025-03-21 20:09:04 -0600

    x

--- Proyecto_ABCC/Proyecto_ABCC/settings.py
+++ Proyecto_ABCC/Proyecto_ABCC/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'Proyecto_ABCC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
+import os
+import dj_database_url
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_bd_2025',
-        'USER': 'nani',
-        'PASSWORD': '7HpQxtOQSNhgnIU48CYanoxp26JSiAKP',
-        'HOST': 'dpg-cvf0u35svqrc73crjnsg-a',
-        'PORT': '5432',
-    }
-    
-    
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
+
+
 
 
 # Password validation
